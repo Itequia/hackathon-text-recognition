2f6ade66497a60afb68b91e96899c1e650c8acb1
Blai Samitier <blai@samitier.me>
2018-02-23 15:10:49 +0100

    server

--- server/text-recognition-bot/bot.js
+++ server/text-recognition-bot/bot.js
@@ -6,7 +6,7 @@ const dialog = require("./dialog")
 const {promisify} = require('util');
 const path = require('path');
 
-const { SLACK_API_TOKEN } = process.env
+const { SLACK_API_TOKEN, APP_URL } = process.env
 
 class ItequiaBot extends SlackBot {
 
@@ -34,17 +34,32 @@ class ItequiaBot extends SlackBot {
 			file: message.file_id
 		})
 		try {
-			let img = await axios.get(resp.file.url_private)
-			await promisify(fs.writeFile)(path.resolve(__dirname, "../public/temp.jpg"), new Buffer(img.data, 'binary').toString('base64'))
-			let file = await promisify(fs.readFile)(path.resolve(__dirname, "../public/temp.jpg"))
+			let img = await axios.request({
+				responseType: 'arraybuffer',
+				url: resp.file.url_private,
+				method: "get"
+			})
+			const response = await computerVision.analyzeWrittenText(img.data)
+			let operationLocation = response.headers["operation-location"]
+			let imageAnalysis = await computerVision.getOperationResult(operationLocation)
+			imageAnalysis = imageAnalysis.data.recognitionResult
+	
 
-			const imageAnalysis = await computerVision.analyzeWrittenText(file)
-			return bot.reply(message, dialog.usage)
+			let file = await promisify(fs.readFile)(path.resolve(__dirname, "../public/index.html"), "utf8")
+			file +=`<script>window.imageAnalysis = ${ JSON.stringify( imageAnalysis ) }</script></body></html>`
+			let filename = "index." + Date.now() + ".html"
+			await promisify(fs.writeFile)(path.resolve(__dirname, "../public") + "/" + filename, file)
+
+			await this.api("chat.postMessage", { 
+				channel: message.user_id,
+				text: "Imagen analizada! La puedes encontrar en: " + APP_URL + filename,
+				as_user: true,
+			})
 		}
 		catch(e) {
 			console.error(e);
+			return bot.reply(message, dialog.error)
 		}
-		return bot.reply(message, dialog.usage)
 	}
 }
 
--- server/text-recognition-bot/computer-vision-api.js
+++ server/text-recognition-bot/computer-vision-api.js
@@ -7,15 +7,32 @@ class ComputerVisionApi {
 		this.axios = axios.create()
 		this.axios.defaults.headers.post['Content-Type'] = 'application/octet-stream'
 		this.axios.defaults.headers.common["Ocp-Apim-Subscription-Key"] = COGNITIVE_SERVICES_API_KEY1
+		this.recognizeTextUrl = `${ COGNITIVE_SERVICES_API_URL }recognizeText?handwriting=true`
 	}
 
-	getApiUrl() {
-		return `${ COGNITIVE_SERVICES_API_URL }recognizeText?handwriting=true`
+	async getOperationResult(url) {
+		return new Promise((resolve, reject) => {
+			let interval = setInterval(async () => {
+				try {
+					console.log("Requesting result...");
+					let result = await this.axios.get(url)
+					if (result.data.status !== "Running") {
+						clearInterval(interval)
+						if (result.data.status === "Succeeded") resolve(result)
+						else reject()
+					}
+				}
+				catch(e) {
+					clearInterval(interval)
+					reject(e)
+				}
+			}, 400)
+		})
 	}
 
 	async analyzeWrittenText(img) {
 		try {
-			return await this.axios.post(this.getApiUrl(), img)
+			return await this.axios.post(this.recognizeTextUrl, img)
 		}
 		catch(e) {
 			console.error(e)
